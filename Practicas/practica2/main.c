65708c03b4970c34c662a6211207da34ebbc9565
root <root@DESKTOP-MQH2AU5.localdomain>
2018-01-22 20:51:42 -0600

    main

--- Practicas/practica2/main.c
+++ Practicas/practica2/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include "operations.h"
+#include "message.h"
+
+#define MAX_STRING_LEN 100
+#define LANG "en"
+
+typedef struct {
+    char *operationName;
+    int(operation *) (int,int);
+} Operation;
 
 int main() {
+    char operation[MAX_STRING_LEN];
+    int operatorOne, operatorTwo;
+    char **messages;
+
+    if( !strcmp(LANG,"en"))
+    {
+        messages = message_EN;
+    }
+
+    
+    if( !strcmp(LANG,"es"))
+    {
+        messages = message_ES;
+    }
+
+    printf("%s\n",messages[0]);
+    scanf("%s", operation);
+    scanf("%d %d", &operatorOne, &operatorTwo);
+    if( !strcmp(operation,"add"))
+    {
+        printf("%s: %d\n", messages[2], add(operatorOne, operatorTwo));
+    } else if ( !strcmp(operation,"substraction"))
+    {
+        printf("%s: %d\n", messages[2], substraction(operatorOne, operatorTwo));
+    } else
+    {
+        printf("%s\n",messages[1]);
+    }
+         
     return 0;
 }
---
+++ Practicas/practica2/message.h
@@ -0,0 +1,11 @@
+char *message_EN[] = {
+    "Wath is the operation",
+    "Operation not implemented yet",
+    "result"
+};
+
+char *message_ES[] = {
+    "Cual es la operación?",
+    "Operacion no implementada todavia",
+    "resultado"
+};
